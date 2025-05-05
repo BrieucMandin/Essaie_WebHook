8cdd7fd0fcb3ef3841aaf563b5c354c22f97365c
Brieuc Mandin <brieucmandin@macbook.local>
2025-05-05 09:28:11 +0200

    ajout notification webhook

---
+++ Football/signals.py
@@ -0,0 +1,40 @@
+# ton_app/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Joueur, Entraineur, Equipe
+import requests
+
+@receiver(post_save, sender=Joueur)
+def notify_webhook_receiver_Joueur(sender, instance, created, **kwargs):
+    payload = {
+        "id" : instance.id,
+        "nom" : instance.nom,
+    }
+    try:
+        requests.post("http://projet-b.local/webhook-endpoint/Joueur/", json=payload)
+    except requests.RequestException as e:
+        print("Erreur webhook :", e)
+
+
+@receiver(post_save, sender=Entraineur)
+def notify_webhook_receiver_Entraineur(sender, instance, created, **kwargs):
+    payload = {
+        "id" : instance.id,
+        "nom" : instance.nom,
+    }
+    try:
+        requests.post("http://projet-b.local/webhook-endpoint/Entraineur/", json=payload)
+    except requests.RequestException as e:
+        print("Erreur webhook :", e)
+
+
+@receiver(post_save, sender=Equipe)
+def notify_webhook_receiver_Equipe(sender, instance, created, **kwargs):
+    payload = {
+        "id" : instance.id,
+        "nom" : instance.nom,
+    }
+    try:
+        requests.post("http://projet-b.local/webhook-endpoint/Equipe/", json=payload)
+    except requests.RequestException as e:
+        print("Erreur webhook :", e)
