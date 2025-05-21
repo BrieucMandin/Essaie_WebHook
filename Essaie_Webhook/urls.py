ca4b7df7de7df73a3c704976a6b8ce9329f44401
Brieuc Mandin <brieucmandin@macbook.local>
2025-05-21 15:47:35 +0200

    ajout de drf-spectacular

--- Essaie_Webhook/urls.py
+++ Essaie_Webhook/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 from Football.views.views_api import JoueurViewSet, EntraineurViewSet, EquipeViewSet
 
 router = DefaultRouter()
@@ -29,4 +31,13 @@ router.register(r"equipes", EquipeViewSet, basename="equipe")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+
+     # Spectacular Schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+
+    # Swagger UI
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+
+    # Redoc UI
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
